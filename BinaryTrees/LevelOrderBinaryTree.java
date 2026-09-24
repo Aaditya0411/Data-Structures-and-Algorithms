d7a61f615eba2cefe73f81f87e5bc6cdc74f7e7c
AadiCode04 <aadigithub61@gmail.com>
2026-09-24 10:28:26 +0530

    BinaryTrees   Added

---
+++ BinaryTrees/LevelOrderBinaryTree.java
@@ -0,0 +1,71 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderBinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    static class BinaryTree {
+
+        static int idx = -1;
+
+        public static Node BuildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+
+            return newNode;
+        }
+
+        public static void LevelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+
+            while (!q.isEmpty()) {
+                Node curr = q.remove();
+
+                System.out.print(curr.data + " ");
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.BuildTree(nodes);
+
+        System.out.println("Level Order:");
+        tree.LevelOrder(root);
+
+    }
+}
